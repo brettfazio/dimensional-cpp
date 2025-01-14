39484cf889b41c09cc51640aa25f2854e21c3e6e
Brett Fazio <brett.fazio@hotmail.com>
2025-01-13 21:56:29 -0500

    format code

--- c/src/dimensional.cpp
+++ c/src/dimensional.cpp
@@ -315,22 +315,22 @@ private:
 
     // Add handling for static_cast
     if (const auto *castExpr = dyn_cast<CXXStaticCastExpr>(E)) {
-        const Expr *subExpr = castExpr->getSubExpr();
-        Unit sourceUnit = inferExpressionUnit(subExpr, Result);
-        
-        // If we're casting from a unit type to a non-unit type, flag it
-        if (sourceUnit.toString() != "unitless") {
-            const auto *destType = castExpr->getType().getTypePtrOrNull();
-            const auto *parent = Result.Nodes.getNodeAs<VarDecl>("var");
-            
-            if (parent && !parent->hasAttr<AnnotateAttr>()) {
-                diag(castExpr->getOperatorLoc(),
-                     "Unit safety violation: cannot drop units through static_cast - "
-                     "casting value with unit %0 to non-unit type")
-                    << sourceUnit.toString();
-            }
+      const Expr *subExpr = castExpr->getSubExpr();
+      Unit sourceUnit = inferExpressionUnit(subExpr, Result);
+
+      // If we're casting from a unit type to a non-unit type, flag it
+      if (sourceUnit.toString() != "unitless") {
+        const auto *destType = castExpr->getType().getTypePtrOrNull();
+        const auto *parent = Result.Nodes.getNodeAs<VarDecl>("var");
+
+        if (parent && !parent->hasAttr<AnnotateAttr>()) {
+          diag(castExpr->getOperatorLoc(),
+               "Unit safety violation: cannot drop units through static_cast - "
+               "casting value with unit %0 to non-unit type")
+              << sourceUnit.toString();
         }
-        return sourceUnit;
+      }
+      return sourceUnit;
     }
 
     if (const auto *initList = dyn_cast<InitListExpr>(E)) {
@@ -368,24 +368,24 @@ private:
     }
 
     if (const auto *castExpr = dyn_cast<CStyleCastExpr>(E)) {
-        const Expr *subExpr = castExpr->getSubExpr();
-        Unit sourceUnit = inferExpressionUnit(subExpr, Result);
-        
-        // Get parent variable if this is an initialization
-        const auto &parents = Result.Context->getParents(*castExpr);
-        if (!parents.empty()) {
-            if (const auto *varDecl = parents[0].get<VarDecl>()) {
-                Unit targetUnit = getUnitFromAnnotations(varDecl);
-                if (targetUnit.toString() != "unitless" && 
-                    targetUnit.toString() != sourceUnit.toString()) {
-                    diag(castExpr->getExprLoc(),
-                         "Unit mismatch in cast: expression has unit %0 but target "
-                         "variable expects unit %1")
-                        << sourceUnit.toString() << targetUnit.toString();
-                }
-            }
+      const Expr *subExpr = castExpr->getSubExpr();
+      Unit sourceUnit = inferExpressionUnit(subExpr, Result);
+
+      // Get parent variable if this is an initialization
+      const auto &parents = Result.Context->getParents(*castExpr);
+      if (!parents.empty()) {
+        if (const auto *varDecl = parents[0].get<VarDecl>()) {
+          Unit targetUnit = getUnitFromAnnotations(varDecl);
+          if (targetUnit.toString() != "unitless" &&
+              targetUnit.toString() != sourceUnit.toString()) {
+            diag(castExpr->getExprLoc(),
+                 "Unit mismatch in cast: expression has unit %0 but target "
+                 "variable expects unit %1")
+                << sourceUnit.toString() << targetUnit.toString();
+          }
         }
-        return sourceUnit;
+      }
+      return sourceUnit;
     }
 
     // Handle literal constants in explicit unit context
